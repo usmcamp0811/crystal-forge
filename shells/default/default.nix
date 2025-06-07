b8fdd0f8cb061be44824eb9db3c1ddb3820cbaf3
Matt Camp <matt@aicampground.com>
2025-06-07 09:54:17 -0500

    wip

--- shells/default/default.nix
+++ shells/default/default.nix
@@ -21,11 +21,19 @@ with lib.crystal-forge;
     shellHook = ''
       echo 🔮 Welcome to the Crystal Forge
 
-      export OPENSSL_DIR=${pkgs.openssl.dev}
+      export OPENSSL_DIR=${pkgs.openssl.out}
+      export OPENSSL_LIB_DIR=${pkgs.openssl.out}/lib
+      export OPENSSL_INCLUDE_DIR=${pkgs.openssl.dev}/include
       export PKG_CONFIG_PATH=${pkgs.openssl.dev}/lib/pkgconfig
 
       export FZF_DEFAULT_OPTS="--height 40% --reverse --border"
-      . ${pkgs.fzf}/share/fzf/key-bindings.bash
-      . ${pkgs.fzf}/share/fzf/completion.bash
+
+      if [ -n "$BASH_VERSION" ]; then
+        . ${pkgs.fzf}/share/fzf/key-bindings.bash
+        . ${pkgs.fzf}/share/fzf/completion.bash
+      elif [ -n "$ZSH_VERSION" ]; then
+        source ${pkgs.fzf}/share/fzf/key-bindings.zsh
+        source ${pkgs.fzf}/share/fzf/completion.zsh
+      fi
     '';
   }
