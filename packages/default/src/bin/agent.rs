6b8d630e1ec6b41db7da5b3d088093512bfc6503
Matt Camp <matt@aicampground.com>
2025-06-07 19:58:44 -0500

    dont care about db on agent

--- packages/default/src/bin/agent.rs
+++ packages/default/src/bin/agent.rs
@@ -7,9 +7,6 @@ use anyhow::Result;
 #[tokio::main]
 async fn main() -> Result<()> {
     let config = config::load_config()?;
-    let db_url = config.database.to_url();
-
-    config::validate_db_connection(&db_url).await?;
     // TODO: Update watch_system to take in the private key path
     system_watcher::watch_system()
 }
