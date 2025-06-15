dbdb32cd117dc49b60e87a165ae3362d839f9462
Matt <gitlab@aicampground.com>
2025-06-15 10:08:45 -0500

    Resolve "Improve Logging for System Derivation Discovery"

--- shells/default/default.nix
+++ shells/default/default.nix
@@ -27,6 +27,20 @@ with lib.crystal-forge;
       export PKG_CONFIG_PATH=${pkgs.openssl.dev}/lib/pkgconfig
 
       export FZF_DEFAULT_OPTS="--height 40% --reverse --border"
+      export RUST_LOG=debug
+      export CRYSTAL_FORGE__DATABASE__HOST=localhost
+      export CRYSTAL_FORGE__DATABASE__PASSWORD=password
+      export CRYSTAL_FORGE__DATABASE__USER=crystal_forge
+      export CRYSTAL_FORGE__FLAKES__WATCHED__dotfiles=https://gitlab.com/usmcamp0811/dotfiles
+      export CRYSTAL_FORGE__SERVER__AUTHORIZED_KEYS__chesty=Asu0Fl8SsM9Pd/woHt5qkvBdCbye6j2Q2M/qDmnFUjc=
+      export CRYSTAL_FORGE__SERVER__AUTHORIZED_KEYS__daly=JhjP4LK72nuTQJ6y7pcYjoTtfrY86BpJBi9WeolcpKY=
+      export CRYSTAL_FORGE__SERVER__AUTHORIZED_KEYS__ermy=z9FINYnz2IPPaECHZbTae5prPFUE/ubAT+4HHLPSq7I=
+      export CRYSTAL_FORGE__SERVER__AUTHORIZED_KEYS__gray=hUwxCZUFydwDjf8BMyXLyMiI33PrKvhfDRj60OkisdY=
+      export CRYSTAL_FORGE__SERVER__AUTHORIZED_KEYS__lucas=OMxvf/rZmi8PZJOpVxjbPHDaX+BmJqp8FUOoosWJ7qY=
+      export CRYSTAL_FORGE__SERVER__AUTHORIZED_KEYS__reckless=SKYgYiwK0vMwK3sJP6R53z0gbtOVSWOmJ33WT4AbCQ8=
+      export CRYSTAL_FORGE__SERVER__AUTHORIZED_KEYS__webb=ZJBA2GS03P+Q2mhUAbjfjFILQ57yGChjXmRdL6Xfang=
+      export CRYSTAL_FORGE__SERVER__HOST=0.0.0.0
+      export CRYSTAL_FORGE__SERVER__PORT=3444
 
       if [ -n "$BASH_VERSION" ]; then
         . ${pkgs.fzf}/share/fzf/key-bindings.bash
