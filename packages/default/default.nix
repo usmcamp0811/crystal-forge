fb38fa4f27819deb9a352ff949421d353a2953da
Matt Camp <matt@aicampground.com>
2025-06-21 12:31:22 -0500

    this builds

--- packages/default/default.nix
+++ packages/default/default.nix
@@ -3,40 +3,22 @@
   pkgs,
   ...
 }: let
-  src = ./.;
-  srcMigrations = src + /migrations;
-
-  sqlx-db =
-    pkgs.runCommand "sqlx-db-prepare" # 3
-    
-    {
-      nativeBuildInputs = [pkgs.sqlx-cli];
-    } ''
-      mkdir $out
-      export DATABASE_URL=sqlite:$out/db.sqlite3
-      sqlx database create
-      sqlx migrate --source ${srcMigrations} run
-    '';
-
-  crystal-forge = pkgs.naersk-lib.buildPackage {
-    inherit src;
-    pname = "crystal-forge";
+  crystal-forge = pkgs.rustPlatform.buildRustPackage {
+    pname = "agent";
     version = "0.1.0";
+    src = ./.;
 
-    doCheck = true;
-    CARGO_BUILD_INCREMENTAL = "false";
-    RUST_BACKTRACE = "full";
-    copyLibs = false;
-
-    overrideMain = old: {
-      # 4
-      linkDb = ''
-        export DATABASE_URL=sqlite:${sqlx-db}/db.sqlite3            # 5
-      '';
-
-      preBuildPhases = ["linkDb"] ++ (old.preBuildPhases or []); # 6
+    cargoLock = {
+      lockFile = ./Cargo.lock;
     };
     nativeBuildInputs = with pkgs; [pkg-config];
+    buildInputs = [
+      pkgs.rustc
+      pkgs.cargo
+      pkgs.pkg-config
+      pkgs.openssl
+      pkgs.sqlx-cli
+    ];
   };
 in
   crystal-forge
