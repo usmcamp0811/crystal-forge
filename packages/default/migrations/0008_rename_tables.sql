8543d5bd6906c8c86c26ef32a1897fb7df792733
Matt Camp <matt@aicampground.com>
2025-07-02 20:54:50 -0500

    rename tables

---
+++ packages/default/migrations/0008_rename_tables.sql
@@ -0,0 +1,8 @@
+ALTER TABLE tbl_commits RENAME TO commits;
+
+ALTER TABLE tbl_evaluation_targets RENAME TO evaluation_targets;
+
+ALTER TABLE tbl_flakes RENAME TO flakes;
+
+ALTER TABLE tbl_system_states RENAME TO system_states;
+
