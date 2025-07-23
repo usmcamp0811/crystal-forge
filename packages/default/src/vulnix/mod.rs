a13e9b560367b725cc1d5a95e43e0afec48921d2
Matt Camp <matt@aicampground.com>
2025-07-23 16:34:48 -0500

    wip

--- packages/default/src/vulnix/mod.rs
+++ packages/default/src/vulnix/mod.rs
@@ -1,8 +1,3 @@
-// pub mod database_scan_results2;
-// pub mod vulnix_parser2;
-// pub mod vulnix_runner2;
-
-// // Re-export commonly used types for convenience
-// pub use database_scan_results2::{DatabaseScanResult, DatabaseScanSummary};
-// pub use vulnix_parser2::{VulnixParser, VulnixScanResult};
-// pub use vulnix_runner2::{VulnixConfig, VulnixRunner};
+pub mod database_scan_results;
+pub mod vulnix_parser;
+pub mod vulnix_runner;
