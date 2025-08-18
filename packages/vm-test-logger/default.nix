d01298e381185c115147933d9ddb8b2cb79e312c
Matt Camp <matt@aicampground.com>
2025-08-17 19:17:22 -0500

    fixed

--- packages/vm-test-logger/default.nix
+++ packages/vm-test-logger/default.nix
@@ -6,13 +6,20 @@
 pkgs.python3Packages.buildPythonPackage rec {
   pname = "vm-test-logger";
   version = "1.0.0";
-  format = "setuptools";
+  format = "pyproject";
 
   src = ./.;
+
+  nativeBuildInputs = with pkgs.python3Packages; [
+    setuptools
+    wheel
+  ];
 
   propagatedBuildInputs = with pkgs.python3Packages; [
     pytest
   ];
+
+  pythonImportsCheck = ["vm_test_logger"];
 
   meta = with lib; {
     description = "Logging utilities for NixOS VM tests";
