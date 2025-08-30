28a31e83695e018402e661b4a7a34b0cf4e6a9d8
Matt Camp <matt@aicampground.com>
2025-08-29 23:37:20 -0500

    no clue but yea runs

--- packages/cf-test-modules/cf_test/scenarios.py
+++ packages/cf-test-modules/cf_test/scenarios.py
@@ -8,7 +8,6 @@ from typing import Any, Dict, List, Optional, Sequence, Tuple
 from . import CFTestClient
 
 
-# FUCK YOU
 def _one_row(client: CFTestClient, sql: str, params: Tuple[Any, ...]) -> Dict[str, Any]:
     rows = client.execute_sql(sql, params)
     return rows[0] if rows else {}
