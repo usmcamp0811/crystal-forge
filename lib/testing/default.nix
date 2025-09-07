7b98a17794b82f51a668abbfaf70deea6c29f5c6
Matt Camp <matt@aicampground.com>
2025-09-06 21:53:43 -0500

    maybe more emmory?

--- lib/testing/default.nix
+++ lib/testing/default.nix
@@ -426,7 +426,7 @@ in rec {
       networking.useDHCP = true;
       networking.firewall.allowedTCPPorts = [port 5432];
       virtualisation.writableStore = true;
-      virtualisation.memorySize = 6096;
+      virtualisation.memorySize = 8096;
       virtualisation.additionalPaths = [systemBuildClosure];
 
       environment.systemPackages = [pkgs.git pkgs.jq pkgs.crystal-forge.default pkgs.crystal-forge.cf-test-modules.runTests pkgs.crystal-forge.cf-test-modules.testRunner];
