7dd12b5ca5266b308a3880d27fdee4b69ac7b617
Matt Camp <matt@aicampground.com>
2025-09-18 20:40:40 -0500

    cleaning up

--- packages/vm-test-logger/vm_test_logger/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-VM Test Logger - Standardized logging for NixOS VM tests
-"""
-
-from .decorators import with_logging
-from .logger import TestLogger, TestPatterns
-
-__version__ = "1.0.0"
-__all__ = ["TestLogger", "TestPatterns", "with_logging"]
