f8f57c65859ef5a0dc3e9eca086bb09b620eb417
Matt Camp <matt@aicampground.com>
2025-09-27 11:39:36 -0500

    wip

--- overlays/default/default.nix
+++ overlays/default/default.nix
@@ -1,7 +1,9 @@
 {
   channels,
   process-compose-flake,
+  nixos-compose,
   ...
 }: final: prev: {
   process-compose-flake = import process-compose-flake.lib {pkgs = final;};
+  nxc = nixos-compose.lib;
 }
--- packages/dev-env/default.nix
+++ packages/dev-env/default.nix
@@ -1,20 +1,18 @@
 {
   lib,
   pkgs,
+  system,
   inputs,
   ...
-}: let
-  # Import nixos-compose from inputs
-  nxc = inputs.nixos-compose;
-in
-  nxc.lib.compose {
-    inherit (pkgs) system;
-    nixpkgs = inputs.nixpkgs;
-    composition = ./composition.nix;
+}:
+pkgs.nxc.compose {
+  inherit system;
+  nixpkgs = pkgs;
+  composition = ./composition.nix;
 
-    # Optional: Add any extra configurations if needed
-    extraConfigurations = [
-      # Add crystal-forge module
-      inputs.self.nixosModules.crystal-forge
-    ];
-  }
+  # Optional: Add any extra configurations if needed
+  extraConfigurations = [
+    # Add crystal-forge module
+    inputs.self.nixosModules.crystal-forge
+  ];
+}
