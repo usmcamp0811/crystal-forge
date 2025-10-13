45fd821d5d0ed1aeb28f4b33abd323bd8de5f911
Matt Camp <matt@aicampground.com>
2025-10-12 19:45:50 -0500

    wip

--- packages/default/migrations/0060_remove_system_fk.sql
+++ packages/default/migrations/0060_remove_system_fk.sql
@@ -1,12 +1,15 @@
--- Add new column
+-- 1) Add column
 ALTER TABLE systems
     ADD COLUMN IF NOT EXISTS desired_derivation_id integer;
 
--- Drop old constraint before adding new one
+-- 2) Drop both old and new constraints (idempotent)
 ALTER TABLE systems
     DROP CONSTRAINT IF EXISTS fk_systems_desired_target;
 
--- Add new foreign key constraint
+ALTER TABLE systems
+    DROP CONSTRAINT IF EXISTS fk_systems_desired_derivation;
+
+-- 3) Add new constraint (clean slate)
 ALTER TABLE systems
     ADD CONSTRAINT fk_systems_desired_derivation FOREIGN KEY (desired_derivation_id) REFERENCES derivations (id);
 
