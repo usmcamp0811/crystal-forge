3ad42e7fe21bd171d8e21dd9ccf473781a1cc04c
Matt Camp <matt@aicampground.com>
2025-10-12 19:45:06 -0500

    wip

--- packages/default/migrations/0060_remove_system_fk.sql
+++ packages/default/migrations/0060_remove_system_fk.sql
@@ -1,11 +1,12 @@
--- 1) add an ID FK to what you actually deploy
+-- Add new column
 ALTER TABLE systems
-    ADD COLUMN IF NOT EXISTS desired_derivation_id int;
+    ADD COLUMN IF NOT EXISTS desired_derivation_id integer;
 
-ALTER TABLE systems
-    ADD CONSTRAINT fk_systems_desired_derivation FOREIGN KEY (desired_derivation_id) REFERENCES derivations (id);
-
--- 2) drop the incompatible text FK
+-- Drop old constraint before adding new one
 ALTER TABLE systems
     DROP CONSTRAINT IF EXISTS fk_systems_desired_target;
+
+-- Add new foreign key constraint
+ALTER TABLE systems
+    ADD CONSTRAINT fk_systems_desired_derivation FOREIGN KEY (desired_derivation_id) REFERENCES derivations (id);
 
