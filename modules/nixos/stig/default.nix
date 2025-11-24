bb9f006646cfa42366c1d440e7ce762378eca2ff
Matt Camp <matt@aicampground.com>
2025-11-23 20:10:49 -0600

    wip

--- modules/nixos/stig/default.nix
+++ modules/nixos/stig/default.nix
@@ -1,5 +1,18 @@
-{
+{lib, ...}: {
+  options.crystal-forge.stig = with lib.types; {
+    active = lib.mkOption {
+      type = attrsOf (attrsOf anything);
+      default = {};
+      description = "Tracking of active STIG controls with their SRG, CCI, and config";
+    };
+    inactive = lib.mkOption {
+      type = attrsOf (attrsOf anything);
+      default = {};
+      description = "Tracking of inactive STIG controls with justifications";
+    };
+  };
+
   imports = [
-    ./banner.nix
+    ./banner
   ];
 }
