1b9b541621f691dd21add02d353486bff0676466
Matt Camp <matt@aicampground.com>
2025-11-24 22:11:33 -0600

    wip

--- modules/nixos/stig/modules/cron/default.nix
+++ modules/nixos/stig/modules/cron/default.nix
@@ -19,7 +19,7 @@ with lib.crystal-forge;
       # https://stigui.com/stigs/Anduril_NixOS_STIG/groups/V-268153
       services.cron = {
         enable = true;
-        systemCronJobs = lib.mkDefault [
+        systemCronJobs = [
           "00 0 * * 0\troot\taide -c /etc/aide/aide.conf --check | /bin/mail -s \"aide integrity check run for ${config.networking.hostName}\" root@notareal.email"
         ];
       };
