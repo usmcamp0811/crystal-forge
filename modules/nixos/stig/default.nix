9779c5c9404382342f9db49112f2eca1d4423530
Matt Camp <matt@aicampground.com>
2025-11-24 18:38:46 -0600

    iwp

--- modules/nixos/stig/default.nix
+++ modules/nixos/stig/default.nix
@@ -1,7 +1,12 @@
-{
-  lib,
-  inputs,
-  ...
-}: {
-  imports = lib.snowfall.fs.get-default-nix-files-recursive ./.;
+{lib, ...}: let
+  fs = lib.snowfall.fs;
+
+  # All default.nix files under this directory
+  allDefaults = fs.get-default-nix-files-recursive ./.;
+
+  # Drop this file itself so we don't self-import
+  submoduleDefaults =
+    builtins.filter (path: path != ./default.nix) allDefaults;
+in {
+  imports = submoduleDefaults;
 }
