503e59d6da84f3473b52ec717fc148447d7476fe
Matt Camp <matt@aicampground.com>
2025-11-25 20:59:24 -0600

    wi

--- modules/nixos/stig/off/default.nix
+++ modules/nixos/stig/off/default.nix
@@ -29,7 +29,7 @@ with lib; {
         pam = disabledControl;
         pwquality = disabledControl;
         sudo = disabledControl;
-        kernel = disabledControl;
+        boot.kernel = disabledControl;
 
         # Access & authentication
         getty = disabledControl;
