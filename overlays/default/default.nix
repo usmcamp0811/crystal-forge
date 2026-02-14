aea69ba0141c17161690da41f889d26f6f79a05f
Matt Camp <matt@aicampground.com>
2026-02-13 21:01:35 -0600

    wip

--- overlays/default/default.nix
+++ overlays/default/default.nix
@@ -1,10 +1,11 @@
-{ channels, process-compose-flake, nixos-compose, ... }:
-final: prev: {
-  process-compose-flake = import process-compose-flake.lib { pkgs = final; };
+{
+  channels,
+  process-compose-flake,
+  nixos-compose,
+  ...
+}: final: prev: {
+  process-compose-flake = import process-compose-flake.lib {pkgs = final;};
   nxc-lib = nixos-compose.lib;
   nxc = nixos-compose.packages.${prev.system}.nixos-compose;
 
-  # Pre-built Chromium from stable nixpkgs (always in binary cache)
-  chromium-stable = stablePkgs.chromium;
 }
-
