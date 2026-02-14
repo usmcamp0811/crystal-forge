a4fa865576a2689447c5d08bb6f84df08d8c532e
Matt Camp <matt@aicampground.com>
2026-02-13 20:28:06 -0600

    wip

--- overlays/default/default.nix
+++ overlays/default/default.nix
@@ -1,24 +1,7 @@
 { channels, process-compose-flake, nixos-compose, ... }:
-final: prev:
-let
-  overrideBatsky = batskyPkg:
-    batskyPkg.overridePythonAttrs (_: {
-      pyproject = true;
-      build-system = [ prev.python3Packages.setuptools ];
-    });
-
-  nurRepos = prev.nur.repos // {
-    kapack = prev.nur.repos.kapack // {
-      batsky = overrideBatsky prev.nur.repos.kapack.batsky;
-    };
-  };
-
-  nurOverlay = prev.nur.override { repos = nurRepos; };
-in {
+final: prev: {
   process-compose-flake = import process-compose-flake.lib { pkgs = final; };
   nxc-lib = nixos-compose.lib;
   nxc = nixos-compose.packages.${prev.system}.nixos-compose;
-
-  nur = nurOverlay;
 }
 
