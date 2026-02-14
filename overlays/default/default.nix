f579c8f88b7e7d1173774be551826166344462ef
Matt Camp <matt@aicampground.com>
2026-02-13 19:25:24 -0600

    wip

--- overlays/default/default.nix
+++ overlays/default/default.nix
@@ -1,10 +1,15 @@
-{
-  channels,
-  process-compose-flake,
-  nixos-compose,
-  ...
-}: final: prev: {
-  process-compose-flake = import process-compose-flake.lib {pkgs = final;};
+{ channels, process-compose-flake, nixos-compose, ... }:
+final: prev: {
+  process-compose-flake = import process-compose-flake.lib { pkgs = final; };
   nxc-lib = nixos-compose.lib;
   nxc = nixos-compose.packages.${prev.system}.nixos-compose;
+
+  python3Packages = prev.python3Packages.override {
+    packageOverrides = pythonFinal: pythonPrev: {
+      batsky = pythonPrev.batsky.overridePythonAttrs (old: {
+        pyproject = true;
+        build-system = with pythonPrev; [ setuptools wheel ];
+      });
+    };
+  };
 }
