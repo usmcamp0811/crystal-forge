f6f31c9969550e329faab330d54b7088b2a001eb
Matt Camp <matt@aicampground.com>
2026-02-25 21:45:44 -0600

    fixed

--- packages/default/src/queries/environments.rs
+++ packages/default/src/queries/environments.rs
@@ -23,7 +23,7 @@ pub struct EnvironmentRow {
 
 /// Fetch the environment record associated with this system
 pub async fn get_environment(pool: &PgPool, id: Uuid) -> Result<Option<Environment>> {
-    let env = sqlx::query_as::<_, Environment>("SELECT * FROM environment WHERE id = $1")
+    let env = sqlx::query_as::<_, Environment>("SELECT * FROM environments WHERE id = $1")
         .bind(id)
         .fetch_optional(pool)
         .await?;
