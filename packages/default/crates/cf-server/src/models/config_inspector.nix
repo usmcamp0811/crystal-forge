950c31fb055c251243c003d89df7b700094729dd
Matt Camp <matt@aicampground.com>
2026-09-05 13:56:24 -0500

    Add isolated config definition value inspection

--- packages/default/crates/cf-server/src/models/config_inspector.nix
+++ packages/default/crates/cf-server/src/models/config_inspector.nix
@@ -1,4 +1,4 @@
-{ flakeRef, configurationName }:
+{ flakeRef, configurationName, encodeValue }:
 
 let
   flake = builtins.getFlake flakeRef;
@@ -65,56 +65,6 @@ let
           priority = definition.priority or null;
         }) option.definitionsWithLocations;
       };
-    };
-
-  encodeValue = depth: declaredType: value:
-    if depth > 16 then {
-      kind = "opaque";
-      value = { type_name = "over_depth"; };
-    }
-    else if builtins.isNull value
-      || builtins.isBool value
-      || builtins.isInt value
-      || builtins.isFloat value
-      || builtins.isString value then {
-        kind = "scalar";
-        inherit value;
-      }
-    else if lib.isDerivation value then {
-      kind = "package";
-      value = {
-        name = value.name or null;
-        pname = value.pname or null;
-        version = value.version or null;
-        output_path = if value ? outPath then builtins.toString value.outPath else null;
-      };
-    }
-    else if builtins.isFunction value then {
-      kind = "opaque";
-      value = { type_name = "lambda"; };
-    }
-    else if builtins.isList value then
-      if builtins.length value > 256 then {
-        kind = "opaque";
-        value = { type_name = "list_over_limit"; };
-      }
-      else {
-        kind = "list";
-        value = map (item: encodeValue (depth + 1) declaredType item) value;
-      }
-    else if builtins.isAttrs value then
-      let names = builtins.attrNames value; in
-      if builtins.length names > 256 then {
-        kind = "opaque";
-        value = { type_name = "attribute_set_over_limit"; };
-      }
-      else {
-        kind = if lib.hasInfix "submodule" declaredType then "submodule" else "attribute_set";
-        value = builtins.mapAttrs (_: item: encodeValue (depth + 1) declaredType item) value;
-      }
-    else {
-      kind = "opaque";
-      value = { type_name = builtins.typeOf value; };
     };
 
   valueFor = entry:
